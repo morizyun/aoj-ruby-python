e788d2550896f56a0115db060c0da2f20216e87f
morizyun <merii.ken@gmail.com>
2014-05-09 08:22:46 +0900

    fix 1137 ruby

--- ruby/1137.rb
+++ ruby/1137.rb
@@ -1,38 +1,36 @@
-n = gets.to_i
-n.times do
-  p = 1
-  sum = 0
-  str = gets.chomp.gsub(/\s/, '')
-  str.chars do |c|
-    if c == 'm'
-      sum += p*1000
-      p = 1
-    elsif c == 'c'
-      sum += p*100
-      p = 1
-    elsif c == 'x'
-      sum += p*10
-      p = 1
-    elsif c == 'i'
-      sum += p
-      p = 1
-    else
-      p = c.to_i
-    end
-  end
+DICT = { 'm' => 1000, 'c' => 100, 'x' => 10, 'i' => 1 }.freeze
 
-  txt = ''
-  h = { 1000 => 'm', 100 => 'c', 10 => 'x', 1 => 'i' }
-  h.each do |k, v|
-    begin
-      i = (sum.to_i/k.to_i).ceil
-      if i == 0
-        next
+def mcxi_to_digit(s)
+  sum = 0
+  DICT.each do |k, v|
+    if s.include?(k)
+      if s[s.index(k)-1] >= '2' and s[s.index(k)-1] <= '9'
+        sum += s[s.index(k)-1].to_i * v
       else
-        txt += i >= 2 ? "#{i}#{v}" : v
-        sum -= i*k.to_i
+        sum += v
       end
     end
   end
-  puts txt
+  return sum
+end
+
+def digit_to_mcxi(n)
+  str = ''
+  DICT.to_a.map do |k, v|
+    val = n/v
+    n = n%v
+    if val >= 2
+      str += "#{val}#{k}"
+    elsif val == 1
+      str += k
+    end
+  end
+  return str
+end
+
+n = gets.to_i
+n.times do
+  a, b = gets.chomp.split(' ')
+  sum = mcxi_to_digit(a) + mcxi_to_digit(b)
+  puts digit_to_mcxi(sum)
 end
