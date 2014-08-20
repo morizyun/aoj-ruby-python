ac6201e129cdcfc0006d9304421cc9f896193e0a
morizyun <merii.ken@gmail.com>
2014-08-20 20:46:53 +0900

    add 0557.rb and 0001&0002.java

---
+++ java/0000/Main.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] a) {
+        for (int i = 1; i < 10; ++i) {
+            for (int j = 1; j < 10; ++j) {
+                System.out.printf(i + "x" + j + "=" + (i*j) + "\n");
+            }
+        }
+    }
+}
--- ruby/0557.rb
+++ ruby/0557.rb
@@ -2,16 +2,16 @@ n = gets.to_i
 arr = gets.split(' ').map(&:to_i)
 answer = arr[n-1]
 
-dp = Array.new(n).map { Array.new(21, 0) }
+dp = n.times.map { [0]*21 }
 dp[0][arr[0]] = 1
 1.upto(n-2) do |i|
   0.upto(20) do |j|
-    next if dp[i-1][j] == 0
-    vp = j + arr[i]
-    vm = j - arr[i]
+    next if dp[i-1][j] <= 0
+    cp = j + arr[i]
+    cm = j - arr[i]
 
-    dp[i][vp] += dp[i-1][j] if vp <= 20
-    dp[i][vm] += dp[i-1][j] if vm >= 0
+    dp[i][cp] += dp[i-1][j] if cp <= 20
+    dp[i][cm] += dp[i-1][j] if cm >= 0
   end
 end
 
